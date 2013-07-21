8f7129e33b2adac39e3c43623f99a40cf980daa7
mcfundash <mcfundash@gmail.com>
2013-07-20 23:41:22 -0700

    made it not crap

--- Qgen.py
+++ Qgen.py
@@ -1,62 +1,48 @@
-import random
-tenper = random.randrange(1,11)
-fifiy = random.randrange(1,3)
+from random import choice
+from random import randrange
+tenper = randrange(1,11)
+fifiy = randrange(1,3)
 quote = ''
-Sciencething = ['grains of sand', 'atoms', 'brains', 'bodies', 'stars', 'electrons', 'molecules', 'software', 'itunes', 'music', 'songs', 'programs']
-Species = ['humans', 'monkeys']
-Person = ['Carl Sagan', 'Gandalf', 'Albert Einstein', 'Oprah', 'Steve Jobs', 'Obama', 'Bill Cosby', 'Darth Vader', 'Richard Stallman', 'Linus Torvalds']
+
+PreWrittenQuotes = ["One, two, three, four, let me hear you scream if you want some more! Like uhh, push it, push it, watch me work it, I'm perfect!", 
+                    'Has anyone really been so far as to go look as decided for?',
+                    'Do you even lift?',
+                    "They don't think it be like it is, but it do",
+                    "Deal with it"]
+
+EvolveVerb = ['evolved', 'come']
+Sciencething = ['grains of sand', 'atoms', 'brains', 'bodies', 'stars', 'electrons', 'molecules', 'software', 'music', 'songs', 'programs']
+Species = ['dogs', 'cats', 'whales', 'chickens', 'dolphins']
+EvSpecies = ['humans', 'monkeys', choice(Species)]
+CharactersOnly = ['Spock', 'Harry', 'Neo', 'Luke', 'Gandalf', 'Darth Vader']
+Person = [choice(CharactersOnly), 'Carl Sagan', 'Albert Einstein', 'Oprah', 'Steve Jobs', 'Obama', 'Bill Cosby', 'Richard Stallman', 'Linus Torvalds']
 Namecall = ['asshole', 'retard', 'faggot']
-def part1(getcase = 0):
-    rand10 = random.randrange(1,11)
-    rand2 = random.randrange(1,3)
-    result = ''
-    if rand2 % 2 == 0:
-        case = 1
-        result += '"There are '
-        if rand2 % 2 == 0:
-            result += 'less '
-        else:
-            result += 'more '
-        result += random.choice(Sciencething) + ' '
-    elif rand2 % 2 != 0:
-        case = 2
-        result += '"If '
-        result += random.choice(Species) + ' evolved from ' + random.choice(Species)
-    if getcase == 1:
-        return case
-    elif getcase == 0:
-        return result
+Entire = ['an entire inch', 'the entire world', 'the entire universe', 'the whole galaxy', 'an entire building', 'a whole continent']
+Teleken = ['force', 'death grip', 'mind meld']
+EndSentence = ['!', '?', '?!', '.', '!1!1!!!11!!', '!!111!!11!', '???', '!!!']
+ListRand = [choice(Sciencething), choice(EvSpecies)]
 
-def part2(getcase = 0):
-    rand1 = random.randrange(1,11)
-    rand2 = random.randrange(1,3) 
-    result = part1()
-    #print part1(1)
-    #print result
-    if part1(1) == 1:
-        if rand2 % 2 ==0:
-            result += 'in your '
-        else:
-            result += 'in all the '
-        result += random.choice(Sciencething) + ' than there are ' + random.choice(Sciencething) + ' in all the ' + random.choice(Sciencething) + '"'
-        case = 1
-    if part1(1) == 2:
-        result += ' than why do ' + random.choice(Species)
-        if rand2 % 2 == 0:
-            result += ' not'
-        result += ' look like ' + random.choice(Species)
-        if rand1 % 5 == 0:
-            result += ', you ' + random.choice(Namecall)
-        result += '?"'
-    if getcase == 1:
-            return case
-        else:
-        return result
-def attribute():
-    result = ' -'
-    result += random.choice(Person)
+def quote1():
+    #rand1 = randrange (1,11)
+    result = 'If ' + choice(EvSpecies) + ' ' + choice(EvolveVerb) + ' from ' + choice(EvSpecies)
+    result +=' then ' + choice(EvSpecies) + ' must have ' + choice(EvolveVerb) + ' from ' +  choice(EvSpecies)
     return result
-#while True:
-quote = str(part2()) + attribute()
-print quote
 
+def quote2():
+    #rand1 = randrange(1,11)
+    Lrand2 = [choice(Entire), choice(ListRand)]
+    morl = ['more ', 'less ']
+    result = 'There are ' + choice(morl) + choice(Sciencething) + ' in ' + choice(Lrand2)
+    result +=' than there are ' + choice(ListRand) + ' in ' + choice(Lrand2)
+    return result
+
+def quote3():
+    result = 'Did you know that if you take ' + str(randrange(1,1000)) + ' ' + choice(ListRand) + ", and put them together, they'd fill up all the space in " + choice(Entire) + '?'
+    return result
+def quote4():
+    result = 'Use the ' + choice(Teleken) + ' ' + choice(CharactersOnly) + choice(EndSentence)
+    return result
+
+ChooseQ = [quote1(), quote2(), quote3(), quote4()]
+
+print '"' + choice([choice(PreWrittenQuotes)] + [choice(ChooseQ)] * 10 ) + '" -' + choice(Person)
